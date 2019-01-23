9fdfabaa9ac56cee6137b1dd558b64a828c4bf54
George Karrys <karrysg@earthlink.net>
2019-01-23 15:10:18 -0700

    add file showing pre and post incrementing

---
+++ Chap3/PreAndPost.cpp
@@ -0,0 +1,20 @@
+// CPP program to demonstrate pre increment 
+// operator. 
+#include <iostream> 
+using namespace std; 
+
+int main() 
+{ 
+	int x = 10, a; 
+
+	a = ++x; 
+	cout << "Pre Increment Operation"; 
+
+	// Value of a will change 
+	cout << "\na = " << a; 
+
+	// Value of x change before execution of a=++x; 
+	cout << "\nx = " << x; 
+
+	return 0; 
+} 
