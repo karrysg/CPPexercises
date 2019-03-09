a406b62132b32ce6907b3698833ca4aa2dd4234c
George Karrys <karrysg@earthlink.net>
2019-03-08 17:10:03 -0700

    Loop.cpp comments Chap 3

---
+++ Chap3/Loop.cpp
@@ -0,0 +1,15 @@
+/**Loop.cpp 
+Write a program that uses a while loop to read words from
+standard input (cin) into a string. This is an "infinite" while loop,
+which you break out of (and exit the program) using a break statement.
+For each word that is read, evaluate it by first using a sequence of if
+statements to "map" an integral value to the word, and then use a switch
+statement that uses that integral value as its selector (this sequence of
+events is not meant to be good programming style; it’s just supposed to
+give you exercise with control flow). Inside each case, print something
+meaningful. You must decide what the "interesting" words are and what the
+meaning is. You must also decide what word will signal the end of the program.
+
+Solution to Chapter 3 Proplem 3
+Thinking in C++
+By Bruce Eckels**/
